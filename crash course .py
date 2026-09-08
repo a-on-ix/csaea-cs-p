2b004da5e4ca68b3329594194e47c45bb81e2718
a-on-ix <kadenbadnone@gmail.com>
2026-09-08 18:22:24 +0000

    my first commiit

---
+++ crash course .py
@@ -0,0 +1,29 @@
+print ("hello world!")
+
+# variable declaration and data types:
+
+a = 10             #integer
+b = 5.5            #float
+c = "CSAEA"        #string
+d = False          #boolean
+
+print(a,b,c,d)
+
+# operators
+# + - / * % ** //
+# += -= /=
+
+e = 100000000 - 10 * 50000 / 99999999 // 22
+print(e)
+e += 7
+print(e)
+
+#f-string
+
+print (f"e is equal {e}")
+
+e -= 7
+print(e)
+
+e ++ 5
+print (f"e is now {e}")
