6013c9cc2226228ef0fbe81560f810b1a6c3cd45
a-on-ix <kadenbadnone@gmail.com>
2026-09-10 16:26:41 +0000

    Crash course day 2

--- crash course .py
+++ crash course .py
@@ -1,29 +1,82 @@
-print ("hello world!")
+import math
 
-# variable declaration and data types:
+# comment
 
-a = 10             #integer
-b = 5.5            #float
-c = "CSAEA"        #string
-d = False          #boolean
+# to comment multiple lines,
+# highlight the lines,
+# and click ctrl + / 
 
-print(a,b,c,d)
+print("Hello world!")
 
-# operators
-# + - / * % ** //
-# += -= /=
+# VARIABLE DECLARATIONS AND DATA TYPES:
 
-e = 100000000 - 10 * 50000 / 99999999 // 22
+a = 4           # integer 
+b = 5.5         # float 
+c = "CSAEA"     # string
+d = False       # boolean 
+
+print(a)
+print(a, b, c, d)
+
+# MATH OPERATORS 
+# + - / *    %  **  //
+# +=   -=   /=   
+
+e = 3 - 1
 print(e)
-e += 7
+e += 20
 print(e)
 
-#f-string
+# f-string, formatted strings
 
-print (f"e is equal {e}")
+print(f"e is equal to {e}")
 
-e -= 7
-print(e)
+e -= 7 
+e += 12
 
-e ++ 5
-print (f"e is now {e}")
+print(f"e is NOW equal to {e}")
+
+# COMPARISONS (booleans, which always return True of False)
+
+#  <   >    <=   >=    ==    !=
+
+print(5 <=  5)
+print(7 == 4)
+print(1 != 2)
+
+isEqual = "Yes" != "YES"
+print(isEqual)
+
+# LOGICAL OPERATORS
+# In order of precedence: not   and   or
+
+f = False
+t = True
+
+#predict output. DOn't run!
+print(not f) # True
+print( f and t) #False
+print(f or t) #True
+print(f or t and not f) # True
+
+# CASTING ()
+
+g = int(5.6536539)
+print(g)
+
+# STRINGS
+
+s1 = "Goodnight"
+s2 = " and "
+s3 = "Goodbye"
+end = s1 + s2+ s3 # concatenation with +
+end += ", Cowboy."
+
+print(end + "\n")
+
+# MATH LIBRARY
+
+print(math.sqrt(14))
+print(math.ceil(3.65))
+print(math.floor(8.94))
+print(math.pow(2, 4))
